25a4ddc3255a8db537d2a50565e9d020bb7328ea
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2023-07-21 22:37:13 -0400

    Initial Commit

---
+++ Movie Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Search
+//
+//  Created by Armen on 2023-07-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
